53135a38abefc29ff3bf32c51a715513c3c8fbb6
Danny Kay <danieljameskay@gmail.com>
2018-07-04 12:40:45 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
 import axios from 'axios';
-import io from 'socket.io-client';
+import { config } from "dotenv";
+
+config();
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
@@ -33,154 +35,149 @@ class App extends React.Component {
 
     componentDidMount() {
 
-      const socket = io(process.env.REACT_APP_SOCKET_SERVER);
+      const socket = new WebSocket(process.env.REACT_APP_SOCKET_SERVER);
 
-      window.onbeforeunload = (e) => {
-        socket.disconnect();
-      };
+      socket.onopen = () => {
+        console.log("CLIENT: Websocket opened to server.");
+        const { lng, lat, zoom } = this.state;
 
-      const { lng, lat, zoom } = this.state;
-
-      const map = new mapboxgl.Map({
-        container: this.mapContainer,
-        //style: 'mapbox://styles/mapbox/streets-v9',
-        style: 'mapbox://styles/danieljameskay/cjgzfw1ac000b2stladr4lg20',
-        center: [lng, lat],
-        zoom
-      });
-
-      map.on('move', () => {
-        const { lng, lat } = map.getCenter();
-
-        this.setState({
-          lng: lng.toFixed(4),
-          lat: lat.toFixed(4),
-          zoom: map.getZoom().toFixed(2)
-        });
-      });
-
-      map.on('load', () => {
-        this.setState({newJob: true})
-
-        // Send to Kafa that the drive is available.
-
-        map.addSource('driver', {
-          "type": "geojson",
-          "data":{
-              "type": "Point",
-              "coordinates": [-73.996207, 40.717573]
-          }
+        const map = new mapboxgl.Map({
+          container: this.mapContainer,
+          //style: 'mapbox://styles/mapbox/streets-v9',
+          style: 'mapbox://styles/danieljameskay/cjgzfw1ac000b2stladr4lg20',
+          center: [lng, lat],
+          zoom
         });
 
-      map.addLayer({
-        "id": "driver",
-        "source": "driver",
-        "type": "circle",
-        "paint": {
-          "circle-radius": 12,
-          "circle-color": "#b5563e"
-        }
-      });  
-
-      // Emits vehicles location every 2 seconds. This needs sending to Kafka.
-      setInterval(() => {
-        socket.emit("currentLocation", `${this.state.driverId}|${map.getSource('driver')._data.coordinates.toString()}|${Date.now()}`)
-
-      }, 2000);
-
-    });
-
-    map.on('click', (e) => {
-
-      if(map.getLayer('route')) {
-        map.removeLayer('route');
-        map.removeSource('route');
-        map.removeLayer('end');
-        map.removeSource('end');
-      }
-
-      const currentPos = map.getSource('driver')._data.coordinates.toString();
-      const destPos = Object.values(e.lngLat).toString();
-
-      // creates the source for the end point
-      map.addSource('end', {
-        "type": "geojson",
-        "data":{
-            "type": "Point",
-            "coordinates": Object.values(e.lngLat)
-        }
-      });
-
-      // creates a layer to display the end point on the map.
-      map.addLayer({
-        "id": "end",
-        "source": 'end',
-        "type": "circle",
-        "paint": {
-          "circle-radius": 12,
-          "circle-color": "#b5563e"
-        }
-      });
-
-      // Make a call to the directions api to get the route information.
-      axios.get(`https://api.mapbox.com/directions/v5/mapbox/driving-traffic/${currentPos};${destPos}?access_token=pk.eyJ1IjoiZGFuaWVsamFtZXNrYXkiLCJhIjoiY2pnZ25xanU5MmRldDMzcGY3ZWhwOWE5biJ9.vEgSv9Vy49SODOG2U5PopA&overview=full&geometries=geojson`)
-        .then((response) => {
-
-          // get the geometry from the data.
-          const route = response.data.routes[0].geometry;
-
-          // Send to Kafa that the drive is not available.
-          this.setState({newJob: false})
-          console.log('Event sent to Kafka: The driver is on its way to a customer.');
-
-          // create a source of data for the route using the payload data.
-          map.addSource('route', {
-            "type": 'geojson',
-            "data": {
-              "type": 'Feature',
-              "geometry": route
-            }
+        map.on('move', () => {
+          const { lng, lat } = map.getCenter();
+      
+          this.setState({
+              lng: lng.toFixed(4),
+              lat: lat.toFixed(4),
+              zoom: map.getZoom().toFixed(2)
           });
+        });
 
-          // create a layer which is going to display the route using the route source for data.
-          map.addLayer({
-            "id": 'route',
-            "type": 'line',
-            "source": 'route',
-            "paint": {
-              "line-color": "#b5563e",
-              'line-width': 6
-            }
-          });
-
-
-          if(this.state.newJob === false) {
-
-            let intervalId = setInterval(() => {
-              map.getSource('driver').setData(this.pointOnMap(route.coordinates[this.state.waypointTracker]));
-              if(this.state.waypointTracker === route.coordinates.length) {
-                console.log('Event sent to Kafka: The driver has reached its destination.')
-                this.setState({waypointTracker: 0})
-                clearInterval(this.state.intervalId);
-                map.removeLayer('route');
-                map.removeSource('route');
-                map.removeLayer('end');
-                map.removeSource('end');
-                console.log('Event sent to Kafka: The driver is ready for a new route.')
-                this.setState({newJob: true})
+        map.on('load', () => {
+          this.setState({newJob: true})
+      
+          // Send to Kafa that the drive is available.
+      
+          map.addSource('driver', {
+              "type": "geojson",
+              "data":{
+                  "type": "Point",
+                  "coordinates": [-73.996207, 40.717573]
               }
+          });
+      
+            map.addLayer({
+              "id": "driver",
+              "source": "driver",
+              "type": "circle",
+              "paint": {
+                "circle-radius": 12,
+                "circle-color": "#b5563e"
+              }
+            });
+      
+            // Emits vehicles location every 2 seconds. This needs sending to Kafka.
+            setInterval(() => {
+              socket.send(JSON.stringify({driverId: this.state.driverId, driverCoordinates: map.getSource('driver')._data.coordinates.toString(), dateTime: Date.now().toString()}));
             }, 2000);
-            this.setState({intervalId})
-          }
-
-
-        })
-        .catch((e) => {
-          console.log(e);
-        })
-
-    });
-
+          });
+      
+          map.on('click', (e) => {
+      
+            if(map.getLayer('route')) {
+              map.removeLayer('route');
+              map.removeSource('route');
+              map.removeLayer('end');
+              map.removeSource('end');
+            }
+      
+            const currentPos = map.getSource('driver')._data.coordinates.toString();
+            const destPos = Object.values(e.lngLat).toString();
+      
+            // creates the source for the end point
+            map.addSource('end', {
+              "type": "geojson",
+              "data":{
+                  "type": "Point",
+                  "coordinates": Object.values(e.lngLat)
+              }
+            });
+      
+            // creates a layer to display the end point on the map.
+            map.addLayer({
+              "id": "end",
+              "source": 'end',
+              "type": "circle",
+              "paint": {
+                "circle-radius": 12,
+                "circle-color": "#b5563e"
+              }
+            });
+      
+            // Make a call to the directions api to get the route information.
+            axios.get(`https://api.mapbox.com/directions/v5/mapbox/driving-traffic/${currentPos};${destPos}?access_token=pk.eyJ1IjoiZGFuaWVsamFtZXNrYXkiLCJhIjoiY2pnZ25xanU5MmRldDMzcGY3ZWhwOWE5biJ9.vEgSv9Vy49SODOG2U5PopA&overview=full&geometries=geojson`)
+              .then((response) => {
+      
+                // get the geometry from the data.
+                const route = response.data.routes[0].geometry;
+      
+                // Send to Kafa that the drive is not available.
+                this.setState({newJob: false})
+                console.log('Event sent to Kafka: The driver is on its way to a customer.');
+      
+                // create a source of data for the route using the payload data.
+                map.addSource('route', {
+                  "type": 'geojson',
+                  "data": {
+                    "type": 'Feature',
+                    "geometry": route
+                  }
+                });
+      
+                // create a layer which is going to display the route using the route source for data.
+                map.addLayer({
+                  "id": 'route',
+                  "type": 'line',
+                  "source": 'route',
+                  "paint": {
+                    "line-color": "#b5563e",
+                    'line-width': 6
+                  }
+                });
+      
+      
+                if(this.state.newJob === false) {
+      
+                  let intervalId = setInterval(() => {
+                    map.getSource('driver').setData(this.pointOnMap(route.coordinates[this.state.waypointTracker]));
+                    if(this.state.waypointTracker === route.coordinates.length) {
+                      console.log('Event sent to Kafka: The driver has reached its destination.')
+                      this.setState({waypointTracker: 0})
+                      clearInterval(this.state.intervalId);
+                      map.removeLayer('route');
+                      map.removeSource('route');
+                      map.removeLayer('end');
+                      map.removeSource('end');
+                      console.log('Event sent to Kafka: The driver is ready for a new route.')
+                      this.setState({newJob: true})
+                    }
+                  }, 2000);
+                  this.setState({intervalId})
+                }
+      
+              })
+              .catch((e) => {
+                console.log(e);
+              })
+      
+            });
+      }
   }
 
   render() {
@@ -189,7 +186,7 @@ class App extends React.Component {
     return (
       <div>
         <div className="inline-block absolute top left mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
-          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom} DriverID: ${this.state.driverId}`}</div>
         </div>
         <div ref={el => this.mapContainer = el} className="absolute top right left bottom" />
       </div>
